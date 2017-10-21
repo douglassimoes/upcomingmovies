790219db7f5c219b38d2bb726142c79b398fcd8f
onkkos <douglassimoesds@gmail.com>
2017-10-21 16:27:43 +0000

    Get response from themoviedb api

--- UpComingMovies/app/src/main/java/com/example/douglas/upcomingmovies/MainActivity.java
+++ UpComingMovies/app/src/main/java/com/example/douglas/upcomingmovies/MainActivity.java
@@ -1,13 +1,69 @@
 package com.example.douglas.upcomingmovies;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.douglas.upcomingmovies.utilities.NetworkUtils;
+
+import org.w3c.dom.Text;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mApiResultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mApiResultTextView = (TextView) findViewById(R.id.tv_api_result);
+    }
+
+    private void makeMovieServiceQuery(){
+        URL movieServiceURL = NetworkUtils.buildUrl();
+        new UpComingMovieServiceTask().execute(movieServiceURL);
+
+    }
+
+    public class UpComingMovieServiceTask extends AsyncTask<URL,Void,String>{
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL moviesUrl = urls[0];
+            String movieServiceResults = null;
+
+            movieServiceResults = NetworkUtils.getResponseFromHttpUrl(moviesUrl);
+            return movieServiceResults;
+        }
+
+        @Override
+        protected void onPostExecute(String movieServiceResults) {
+            if(movieServiceResults != null && !movieServiceResults.equals("")){
+                mApiResultTextView.setText(movieServiceResults);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.action_search){
+            makeMovieServiceQuery();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ UpComingMovies/app/src/main/java/com/example/douglas/upcomingmovies/utilities/NetworkUtils.java
@@ -0,0 +1,71 @@
+package com.example.douglas.upcomingmovies.utilities;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Douglas on 21/10/2017.
+ */
+
+public class NetworkUtils {
+    final static String UP_COMMING_MOVIES_BASE_URL = "https://api.themoviedb.org/3/movie/upcoming";
+    final static String API_KEY = "1f54bd990f1cdfb230adb312546d765d";
+
+    final static String PARAM_API_KEY = "api_key";
+    final static String PARAM_LANGUAGE = "language";
+    final static String PARAM_PAGE = "page";
+
+    public static URL buildUrl(){
+        Uri builtUri = Uri.parse(UP_COMMING_MOVIES_BASE_URL).buildUpon()
+                .appendQueryParameter(PARAM_API_KEY, API_KEY)
+                .appendQueryParameter(PARAM_LANGUAGE, "en-US")
+                .appendQueryParameter(PARAM_PAGE, "1")
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            Log.e("movies","Build url failed.");
+            e.printStackTrace();
+        }
+        return url;
+    }
+
+    public static String getResponseFromHttpUrl(URL moviesUrl){
+        HttpURLConnection urlConnection = null;
+        String result=null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) moviesUrl.openConnection();
+            inputStream = urlConnection.getInputStream();
+            String jsonDefault = "UTF-8";
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, jsonDefault), 8);
+            StringBuilder sb = new StringBuilder();
+
+            String line = null;
+            while ((line = reader.readLine()) != null)
+            {
+                sb.append(line + "\n");
+            }
+            result = sb.toString();
+            return result;
+
+        } catch (IOException e) {
+            Log.e("movies", "Response from http failed.");
+            e.printStackTrace();
+        }finally {
+            urlConnection.disconnect();
+        }
+
+        return null;
+    }
+}
